85f117b0a4afe767cfc9ea4c52be6fc3a45d505e
SmitasCodes <airidassmith30@gmail.com>
2023-08-04 00:16:33 +0300

    added service and contact form is now functional

--- src/sections/Projects/Modal/Carousel.jsx
+++ src/sections/Projects/Modal/Carousel.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // All the controls of carousel
+  // All the controls of carousel down below
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
@@ -14,10 +14,6 @@ const Carousel = ({ images }) => {
     const isLastSlide = currentIndex === images.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
   };
 
   return (
