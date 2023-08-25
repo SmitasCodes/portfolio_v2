bdd39719b253b07ad5faed82aebd47f45154a557
SmitasCodes <airidassmith30@gmail.com>
2023-08-25 15:32:46 +0300

    added quiz project

--- src/sections/Projects/ProjectsData/projectsData.js
+++ src/sections/Projects/ProjectsData/projectsData.js
@@ -17,7 +17,7 @@ export const projectsData = [
     title: "Events Page",
     technologies: "React, Bootstrap, CSS",
     description:
-      "This is a school project where we were tasked with creating a website where users can browse events and save them. There are two roles: admin and user. Every user can post events, but only admins can approve them. I was specifically tasked with developing most of the backend, implementing authentication on the page, setting up event filtering, and creating an admin panel. Although this page has numerous bugs, and I'm not particularly satisfied with its performance ,but since I put a lot of work into it, I decided to include it as one of my projects. ",
+      "This is a school group project where we were tasked with creating a website where users can browse events and save them. There are two roles: admin and user. Every user can post events, but only admins can approve them. I was specifically tasked with developing most of the backend, implementing authentication on the page, setting up event filtering, and creating an admin panel. Although this page has numerous bugs, and I'm not particularly satisfied with its performance ,but since I put a lot of work into it, I decided to include it as one of my projects. ",
     coverImage: new URL("./Events/img_1.png", import.meta.url).href,
     vercel: "https://events-frontend-project.vercel.app/",
     github: "https://linkmix.co/18368871",
@@ -27,6 +27,20 @@ export const projectsData = [
       new URL("./Events/img_3.png", import.meta.url).href,
       new URL("./Events/img_4.png", import.meta.url).href,
       new URL("./Events/img_5.png", import.meta.url).href,
+    ],
+  },
+  {
+    title: "Quiz",
+    technologies: "Javascript, Bootstrap",
+    description:
+      "This was a school group project in which we were assigned the task of creating a quiz using a provided array of questions. I served as the team lead for this project and was responsible for overall bug fixing, ensuring the questions appeared correctly, and making sure that the bubbles below the questions changed dynamically based on the selected answers.",
+    coverImage: new URL("./Quiz/img_2.png", import.meta.url).href,
+    vercel: "https://quiz-app-teal-kappa.vercel.app/",
+    github: "https://github.com/SmitasCodes/quiz_app",
+    images: [
+      new URL("./Quiz/img_1.png", import.meta.url).href,
+      new URL("./Quiz/img_2.png", import.meta.url).href,
+      new URL("./Quiz/img_3.png", import.meta.url).href,
     ],
   },
   {
